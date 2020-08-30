46402c39eb0921694c989a8cd254c8b4c807b27e
bonnie <bonnie.developer@gmail.com>
2020-08-30 19:52:20 +0530

    Finished barebone Directory class. Added .gitignore

--- Experiment 3 - Directory/two_level_directory.py
+++ Experiment 3 - Directory/two_level_directory.py
@@ -1,9 +1,10 @@
 class Directory:
 
-    def __init__(self, name):
+    def __init__(self, name, level):
         self.name = name
         self.files = []
         self.directories = []
+        self.level = level
 
     def getName(self):
         return self.name
@@ -12,24 +13,48 @@ class Directory:
         self.files.append(filename)
     
     def mkdir(self, directory_name):
-        self.directories.append(Directory(directory_name))
+        current_level = self.level + 1
+        self.directories.append(Directory(directory_name, current_level))
 
     def ls(self):
         for file in self.files:
             print(file, end="  ")
         for directory in self.directories:
             print(directory.name, end="  ")
+        print()
     
+    def open(self, directory_name):
+        found = 0
+        for directory in self.directories:
+            if (directory.name == directory_name):
+                found = 1
+                directory.ls()
+                break
+        if(not found):
+            print("Error: Directory doesn't exist")
+
+    def touchInside(self, directory_name, filename):
+        found = 0
+        for directory in self.directories:
+            if (directory.name == directory_name):
+                found = 1
+                directory.touch(filename)
+                break
+        if(not found):
+            print("Error: Directory doesn't exist")
+
 
 
 
 def main():
-    root = Directory("bin")
+    root = Directory("bin", 1)
     current_dir = root
     root.touch('main.py') 
     root.touch('fast.py')
-    root.mkdir('projectsofmine')
+    root.mkdir('second')
     root.ls()
+    root.touchInside('second', 'project1')
+    root.open('second')
     print()
 
 
