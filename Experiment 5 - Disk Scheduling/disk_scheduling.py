20a897d71587e5a71e962717bd9a6d75e65b0b7c
bonnie <bonnie.developer@gmail.com>
2020-09-11 14:32:54 +0530

    FCFS complete

--- Experiment 5 - Disk Scheduling/disk_scheduling.py
+++ Experiment 5 - Disk Scheduling/disk_scheduling.py
@@ -1,22 +1,38 @@
 import sys
 
 
-def print_req(request):
-    for req in request:
+def print_req(request_queue):
+    for req in request_queue:
         print(req, end="  ")
     print()
+
+def head_movement_path(head_movement):
+    for item in head_movement:
+        print(item, end="-> ")
+    print("END")
+
+
+def fcfs(request_queue, head):
+    movement = 0
+    prev_head = head
+    for req in request_queue:
+        movement += abs(req - prev_head)
+        # print(req, " - ", prev_head, "= ", abs(req - prev_head), " | total movement = ", movement)
+        prev_head = req
+    head_movement_path(request_queue)
+    print("Total Head Movement = ", movement)
 
 
 def main():
     print("Enter the no. of request : ")
     n = int(input())
     print("Provide the positions to visit (max:200) : ")
-    request = []
+    request_queue = []
     for i in range(n):
         req_pos = int(input())
-        request.append(req_pos)
-    head = request[0]
-    print_req(request)
+        request_queue.append(req_pos)
+    head = request_queue[0]
+    fcfs(request_queue, head)
     
 
 
