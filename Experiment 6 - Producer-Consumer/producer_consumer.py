109dcaed1022b13b8f15c8b3d4a824c623bd8b3c
bonnie <bonnie.developer@gmail.com>
2020-09-18 21:02:42 +0530

    Started with producer_consumer

---
+++ Experiment 6 - Producer-Consumer/producer_consumer.py
@@ -0,0 +1,44 @@
+def print_buffer(buffer):
+    print("Buffer = ", end="")
+    for item in buffer:
+        if(item == 0):
+            print("-", end="  ")
+            continue
+        print(item, end="  ")
+    print()
+
+def main():
+    i=-1;
+    n = int(input("Enter the buffer size : "))
+    buffer = [0 for i in range(n)]
+    print("type 'help' for command")
+    while(True):
+        user_input = input()
+        command = user_input.split()[0]
+        try:
+            data = int(user_input.split()[1])
+        except:
+            pass
+        if(command == "produce"):
+            if(i < n-1):
+                i += 1
+                buffer[i] = data
+                print_buffer(buffer)
+            else:
+                print("Buffer Full!")
+        elif(command == "consume"):
+            if(i >= 0):
+                print("Data consumed is ", buffer[i])
+                buffer[i] = 0
+                i -= 1
+                print_buffer(buffer)
+            else:
+                print("Buffer is empty!")
+        elif(command == 'help'):
+            print("%-20s %-20s" % ("produce <data>", "produces <data> into buffer"))
+            print("%-20s %-20s" % ("consume", "produces <data> into buffer"))
+        else:
+            print("Enter valid command mishter")
+
+
+main()
